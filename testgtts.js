128d5a57521a1712b01dde711e5ecc4f5c8f03e3
Br!z@l0k@ <57237342+ianmsfvenom@users.noreply.github.com>
2021-01-31 00:22:07 -0300

    Delete testgtts.js

--- testgtts.js
+++
@@ -1,9 +0,0 @@
-const { getRandom } = require('./lib/functions')
-const tts = require('gtts')
-var gtts = new tts('ola amigos', 'pt');
-ranm = getRandom('.mp3')
-rano = getRandom('.ogg')
-gtts.save(ranm, function (err, result){
-    if(err) return console.log('erro')
-    console.log('deu certo')
-})
