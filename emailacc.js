b911a5aafb2315c25daf504890934545b1d9a4a6
Br!z@l0k@ <57237342+ianmsfvenom@users.noreply.github.com>
2021-02-18 19:48:40 -0300

    Add files via upload

--- emailacc.js
+++ emailacc.js
@@ -7,3 +7,12 @@ exports.username = username
 exports.passw = passw
 
 //ATENÇÃO AQUI E A CONTA DE E-MAIL DO BOT SE QUISER QUE O COMANDO unbanwpp FUNCIONE CRIE UMA CONTA GMAIL PARA O BOT
+
+//LOCALIZAÇÃO DO FFMPEG, EXCLUA ESSAS 2 BARRAS E O NOME DA PLATAFORMA QUE VOCÊ NÃO USAR
+
+// PC
+// const ffmpeg = 'C:/webm/bin/ffmpeg.exe'
+
+// TERMUX
+const ffmpeg = '/data/data/com.termux/files/usr/bin/ffmpeg'
+exports.ff = ffmpeg
