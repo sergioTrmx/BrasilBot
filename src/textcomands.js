061ebcd671393fe2944602f3b7804c896ec49454
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-03-19 00:40:46 -0600

    Add files via upload

---
+++ src/textcomands.js
@@ -0,0 +1,15 @@
+const textcomands = (p) => {
+return `𝕾𝖊𝖏𝖆𝖒 𝕭𝖊𝖒-𝖁𝖎𝖓𝖉𝖔𝖘 𝖆𝖔 𝕸𝖊𝖓𝖚 𝖉𝖊 𝕿𝖊𝖝𝖙𝖔 𝖉𝖔
+💎 ▁▂▃༻ S̸̲̓̏̿̑e̴̱̮̼͔̐͋̃́͝ṙ̸̨͇̥̰̻́́̈͜ǧ̸͈̦̗̖̆̿͌̄͑̓͠i̵͍̬͆ö̵̯͎͚̀̿̋͑͛͛̉̚-̷̛͙̹͓͈̺̯̥̿͛̃̓B̴̛̺͓̞͚͖̞̤͙͔́̽̏͒̈̐ő̶̡̧̬͖͓͎̪̻̝̒̂̆̂̄̒̕͘t̶̢̻̬́̄͝ ༺▃▂▁ 💎
+╔════ ❯ 𝓬𝓶𝓭 𝓭𝓮 𝓽𝓮𝔁𝓽𝓸 ❮ ══════════
+║ ❯ *${p}glitch* <text1> | <text2>
+║ ❯ *${p}ravetext* <text1> | <text2>
+║ ❯ *${p}woodtext* <text1> | <text2>
+║ ❯ *${p}neon* <text1> | <text2>
+║ ❯ *${p}sunset* <text1> | <text2>
+║ ❯ *${p}gimage* <text>
+║ ❯ *${p}whatis* <text>
+║ ❯ *${p}text3d* <text>
+╚═══════════════════════════`
+}
+exports.textcomands = textcomands
