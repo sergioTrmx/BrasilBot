9369787c6ae0d5b9bc242fb776fd586530041730
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-03-19 00:42:07 -0600

    Add files via upload

---
+++ src/welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `ola pana @${number}. bienvenido al grupo: ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `adios joto @${number}. ya estorbabas👋`
+}
+exports.bye = bye
