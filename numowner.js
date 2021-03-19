7f3fe39454dce70417523273f81eb8507e32cecc
sergioTrmx <80510599+sergioTrmx@users.noreply.github.com>
2021-03-19 00:37:59 -0600

    Add files via upload

---
+++ numowner.js
@@ -0,0 +1,12 @@
+const ownerNumber = ["525623741080@s.whatsapp.net"]
+const OriginalOwner = '525623741080'
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Mi Creador^~^\n' 
+            + 'ORG:Ian;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=525623741080:+52 56 2374-1080\n' 
+            + 'END:VCARD'
+
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
